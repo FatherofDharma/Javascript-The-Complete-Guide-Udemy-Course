270f9f75176fe42165c6c51d95d642e41163b9de
FatherofDharma <fatherofdharma@gmail.com>
2020-03-25 17:53:40 -0700

    complete assignment

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,13 @@
+let userInput = 30;
+let result;
+
+result = userInput + 18;
+
+result = result * 2;
+
+result = result - 3;
+
+result = result / 2;
+
+alert("Your user input is " + userInput);
+alert("Your result is " + result);
