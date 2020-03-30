e35099449de997f8e9abf1fd065d65cd1eca414e
FatherofDharma <fatherofdharma@gmail.com>
2020-03-30 15:27:21 -0700

    follow lesson

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,13 @@
 const defaultResult = 0;
-let currentResult = 0;
+let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
 
-let calculationDescription = `( ${defaultResult} + 10) * 3/3-1`;
+currentResult = add(1, 2);
 
-outputResult(currentResult, errorMessage);
+let calculationDescription = `( ${defaultResult} + 10) * 3/2-1`;
+
+outputResult(currentResult, calculationDescription);
